a7f3f62ccb8b0432aedfd01eb395714ea4cfd660
marniatitriningsitamoama07 <marniatitriningsitamoama@gmail.com>
2023-03-08 20:28:43 +0700

    Project Magang 1

--- routes/web.php
+++ routes/web.php
@@ -16,44 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return 9 * 9;
-});
-// Route::get('/about', function () {
-//     return view('whatever');
+// Route::get('/', function () {
+//     return 9 * 9;
 // });
-Route::get('/contact', function () {
-    return view('contact', ['name' => 'project', 'phone' => '081236....']);
-});
+// // Route::get('/about', function () {
+// //     return view('whatever');
+// // });
+// Route::get('/contact', function () {
+//     return view('contact', ['name' => 'project', 'phone' => '081236....']);
+// });
 
-Route::redirect('/contact', '/contact-us');
+// Route::redirect('/contact', '/contact-us');
 
-Route::get('/product', function () {
-    return 'product';
-});
+// Route::get('/product', function () {
+//     return 'product';
+// });
 
-Route::get('/product/{id}', function ($id) {
-    return view('product.detail', ['id' => $id]);
-});
+// Route::get('/product/{id}', function ($id) {
+//     return view('product.detail', ['id' => $id]);
+// });
 
-Route::prefix('admin')->group(function () {
+// Route::prefix('admin')->group(function () {
 
-    Route::get('/profile-admin', function () {
-        return 'profile admin';
-    });
-    Route::get('/about-admin', function () {
-        return 'about admin';
-    });
-    Route::get('/contact-admin', function () {
-        return 'contact admin';
-    });
-    Route::get('/profile-admin2', function () {
-        return 'profile admin';
-    });
-    Route::get('/about-admin2', function () {
-        return 'about admin2';
-    });
-    Route::get('/contact-admin2', function () {
-        return 'contact admin';
-    });
-});
+//     Route::get('/profile-admin', function () {
+//         return 'profile admin';
+//     });
+//     Route::get('/about-admin', function () {
+//         return 'about admin';
+//     });
+//     Route::get('/contact-admin', function () {
+//         return 'contact admin';
+//     });
+//     Route::get('/profile-admin2', function () {
+//         return 'profile admin';
+//     });
+//     Route::get('/about-admin2', function () {
+//         return 'about admin2';
+//     });
+//     Route::get('/contact-admin2', function () {
+//         return 'contact admin';
+//     });
+// });
